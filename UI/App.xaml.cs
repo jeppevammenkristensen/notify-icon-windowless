a719634df4f090f4eeadf5dbfadee98c92fb8058
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2024-02-15 22:19:55 +0100

    Inital commit

---
+++ UI/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using H.NotifyIcon;
+
+namespace UI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private TaskbarIcon? notifyIcon;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+        notifyIcon = (TaskbarIcon) FindResource("NotifyIcon");
+        notifyIcon.ForceCreate();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        notifyIcon?.Dispose(); //the icon would clean up automatically, but this is cleaner
+        base.OnExit(e);
+    }
+}
---
+++ UI/NotifyIconViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using H.NotifyIcon;
+
+namespace UI;
+
+public partial class NotifyIconViewModel : ObservableRecipient, IRecipient<MainWindow.MainVisibilityChanged>
+{
+    public NotifyIconViewModel()
+    {
+        IsActive = true;
+    }
+    
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(ShowWindowCommand))]
+    public bool canExecuteShowWindow = true;
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(HideWindowCommand))]
+    public bool canExecuteHideWindow;
+    
+    // <summary>
+    /// Shows a window, if none is already open.
+    /// </summary>
+    [RelayCommand(CanExecute = nameof(CanExecuteShowWindow))]
+    public void ShowWindow()
+    {
+        Application.Current.MainWindow ??= new MainWindow();
+        Application.Current.MainWindow.Show(true);
+        CanExecuteShowWindow = false;
+        CanExecuteHideWindow = true;
+    }
+
+    /// <summary>
+    /// Hides the main window. This command is only enabled if a window is open.
+    /// </summary>
+    [RelayCommand(CanExecute = nameof(CanExecuteHideWindow))]
+    public void HideWindow()
+    {
+        Application.Current.MainWindow.Hide(true);
+        CanExecuteShowWindow = true;
+        CanExecuteHideWindow = false;
+    }
+
+    /// <summary>
+    /// Shuts down the application.
+    /// </summary>
+    [RelayCommand]
+    public void ExitApplication()
+    {
+        Application.Current.Shutdown();
+    }
+
+    public void Receive(MainWindow.MainVisibilityChanged message)
+    {
+        if (message.Hide)
+        {
+            CanExecuteShowWindow = true;
+            CanExecuteHideWindow = false;
+        }
+    }
+}
